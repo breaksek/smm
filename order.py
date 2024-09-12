872fbfd34d8e98d85280e22cdad85329572261d2
breaksek <98497506+breaksek@users.noreply.github.com>
2024-09-12 17:43:34 +0700

    Create order.py

---
+++ order.py
@@ -0,0 +1,21 @@
+import requests
+
+def connect(end_point, post):
+    headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)'}
+    response = requests.post(end_point, data=post, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    return False
+
+# Contoh membuat pesanan
+api_url = 'api_url'
+post_data = {
+    'api_key': 'randomkey',
+    'action': 'order',
+    'secret_key': 'secret_key',
+    'service': 1,
+    'data': 'nasa',
+    'quantity': 150
+}
+api_response = connect(api_url, post_data)
+print(api_response)
