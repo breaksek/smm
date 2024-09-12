39216c4be183bdffca237fc109831948065de714
breaksek <98497506+breaksek@users.noreply.github.com>
2024-09-12 18:17:51 +0700

    Create statusorder.js

---
+++ statusorder.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+async function connect(end_point, post) {
+    try {
+        const headers = {
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)'
+        };
+        const response = await axios.post(end_point, new URLSearchParams(post), { headers });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+// Contoh mengecek status pesanan
+api_url = 'api_url';
+post_data = {
+    api_key: 'randomkey',
+    secret_key: 'secret_key',
+    action: 'status',
+    id: '9'
+};
+connect(api_url, post_data).then(api => console.log(api));
