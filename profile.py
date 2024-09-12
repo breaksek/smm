0095e7ac7f64f829deb8e7fb915efbd733bffc2c
breaksek <98497506+breaksek@users.noreply.github.com>
2024-09-12 20:01:38 +0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import urllib.parse
+
 try:
     import requests
     import os
 except Exception as k:
-    exit(f"\n [•] Modul {k} tidak ditemukan")
+    print(f"\n [•] Kesalahan Modul :{k}")
+    crot = urllib.parse.quota_plus(f"Error type : {k}")
+    os.system(f"xdg-open https://api.whatsapp.com/6281331184338?text={x}")
+    exit(1)
 
 try:
     import data as data
-except Exception as ko:
-    exit(f" [•] File {ko}.py Tidak ada")
+except Exception as k:
+    print(f" [•] Kesalahan File {k}")
+    crot = urllib.parse.quota_plus(f"Error type : {k}")
+    os.system(f"xdg-open https://api.whatsapp.com/6281331184338?text={k}")
+    exit(1)
     
 def connect(end_point, post):
     headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)'}
