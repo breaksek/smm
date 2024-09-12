53e95b825f9ad6038a3f154e4811d05b49e5748d
breaksek <98497506+breaksek@users.noreply.github.com>
2024-09-12 19:08:57 +0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,17 +1,24 @@
 import requests
+import data as data
 
 def connect(end_point, post):
     headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)'}
     response = requests.post(end_point, data=post, headers=headers)
     if response.status_code == 200:
         return response.json()
+    else:
+        print(f"Error: {response.status_code}")
     return False
 
-api_url = 'api_url'
-post_data = {
-    'api_key': 'randomkey',
-    'secret_key': 'secret_key',
-    'action': 'profile'
-}
-api_response = connect(api_url, post_data)
-print(api_response)
+def get_profile():
+    api_url = data.api_url
+    post_data = {
+        'api_key': data.api_key,
+        'secret_key': data.secret_key,
+        'action': 'profile'
+    }
+    api_response = connect(api_url, post_data)
+    print(api_response)
+
+if __name__ == "__main__":
+    get_profile()
